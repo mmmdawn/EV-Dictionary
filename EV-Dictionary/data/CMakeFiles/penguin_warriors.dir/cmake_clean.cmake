eed18902bc10ac4b058de8d556235036cd681a42
Dawn <congminh292k@gmail.com>
2020-06-11 12:10:39 +0700

    Final

---
+++ EV-Dictionary/data/CMakeFiles/penguin_warriors.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/penguin_warriors.dir/main.c.o"
+  "penguin_warriors"
+  "penguin_warriors.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/penguin_warriors.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
