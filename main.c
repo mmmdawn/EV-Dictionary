ce7b27b5a4f21a6ab20aee5396e113e66d46abdb
Dawn <congminh292k@gmail.com>
2020-06-11 13:27:24 +0700

    Bình tự sửa lỗi

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 #include <string.h>
 #include "./sqlite3/sqlite3.h"
 
-GtkWidget *textview1,*textview2,*textview3,*textview4;
+GtkWidget *textview_meaning,*textview_history,*textview_add,*textview_edit;
 GtkWidget *searchentry;
-GtkWidget *entry1,*entry2, *entry3;
+GtkWidget *entry_add,*entry_edit, *entry_delete;
 GtkWidget *window_main,*window_add, *window_delete, *window_edit, *window_quit, *window_help, *window_about;
 
 GtkListStore *list;
@@ -46,26 +46,26 @@ int callback_check(void *Notused, int argc, char **argv, char **azColName);
 
 void set_textview_text(char * text) {
     GtkTextBuffer *buffer;
-    buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview1));
+    buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview_meaning));
     if (buffer == NULL) {
         printf("Get buffer fail!");
         buffer = gtk_text_buffer_new(NULL);
     }
     gtk_text_buffer_set_text(buffer, text, -1);
-    gtk_text_view_set_buffer(GTK_TEXT_VIEW(textview1), buffer);
+    gtk_text_view_set_buffer(GTK_TEXT_VIEW(textview_meaning), buffer);
 }
 
-void set_history_textview_text(char * text) { //ko hay chinh sua sau 
+void set_history_textview_text(char * text) {
     GtkTextBuffer *buffer;
     GtkTextIter st_iter;
     GtkTextIter ed_iter;  
-    buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview2));
+    buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview_history));
     gtk_text_buffer_set_text(buffer, text, -1);
-    gtk_text_buffer_get_start_iter (gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview2)), &st_iter);
-    gtk_text_buffer_get_end_iter (gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview2)), &ed_iter);
-    char *histext =  gtk_text_buffer_get_text(gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview2)), &st_iter, &ed_iter, FALSE);
-    gtk_text_buffer_set_text(buffer,histext, -1);
-    gtk_text_view_set_buffer(GTK_TEXT_VIEW(textview2), buffer);
+    gtk_text_buffer_get_start_iter (gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview_history)), &st_iter);
+    gtk_text_buffer_get_end_iter (gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview_history)), &ed_iter);
+    char *historytext =  gtk_text_buffer_get_text(gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview_history)), &st_iter, &ed_iter, FALSE);
+    gtk_text_buffer_set_text(buffer,historytext, -1);
+    gtk_text_view_set_buffer(GTK_TEXT_VIEW(textview_history), buffer);
 }
 
 void press_search()
@@ -77,12 +77,13 @@ void press_search()
 
 void search_keyword(char *word)
 {
+  if ( strcmp(word,"") == 0 )
+     set_textview_text("Bạn chưa nhập gì !");
   exist = NULL;
   sprintf(sql,"SELECT * FROM Dictionary WHERE key_word = \"%s\" ;" ,word);
   sqlite3_exec(db, sql, callback_search, 0, &err_msg);
   if ( exist == NULL )
-     set_textview_text("\nRất tiếc, từ này hiện không có trong từ điển."
-                             "\nNhấn nút \"Thêm từ\" để bổ sung vào từ điển.");
+     set_textview_text("Từ này hiện không có trong từ điển.\nẤn nút\"Thêm từ\" để bổ sung vào từ điển.");
 }
 void enter_keyword(GtkWidget *widget, GdkEventKey *event, gpointer user_data)
 {
@@ -157,8 +158,8 @@ void press_add()
     window_add = GTK_WIDGET(gtk_builder_get_object(builder, "window_add"));
     gtk_builder_connect_signals(builder, NULL);
 
-    entry1 = GTK_WIDGET(gtk_builder_get_object(builder, "entry1"));
-    textview3 = GTK_WIDGET(gtk_builder_get_object(builder, "textview3"));
+    entry_add = GTK_WIDGET(gtk_builder_get_object(builder, "entry_add"));
+    textview_add = GTK_WIDGET(gtk_builder_get_object(builder, "textview_add"));
 
     g_object_unref(builder);
     gtk_widget_show(window_add);
@@ -169,19 +170,19 @@ void click_ok_add()
     GtkTextIter st_iter;
     GtkTextIter ed_iter;
     char word[50];
-    strcpy(word,gtk_entry_get_text(GTK_ENTRY(entry1)));
-    gtk_text_buffer_get_start_iter (gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview3)), &st_iter);
-    gtk_text_buffer_get_end_iter (gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview3)), &ed_iter);
-    char *nghia =  gtk_text_buffer_get_text(gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview3)), &st_iter, &ed_iter, FALSE);
+    strcpy(word,gtk_entry_get_text(GTK_ENTRY(entry_add)));
+    gtk_text_buffer_get_start_iter (gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview_add)), &st_iter);
+    gtk_text_buffer_get_end_iter (gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview_add)), &ed_iter);
+    char *meaning =  gtk_text_buffer_get_text(gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview_add)), &st_iter, &ed_iter, FALSE);
     exist = NULL ;
     sprintf(sql,"SELECT * FROM Dictionary WHERE key_word = \"%s\" ;" ,word);
     sqlite3_exec(db, sql, callback_check, 0, &err_msg);
     if (exist != NULL) {
-        show_message(window_add, GTK_MESSAGE_ERROR, "Xảy ra lỗi!", "Từ đã tồn tại trong từ điển.");
+        show_message(window_add, GTK_MESSAGE_ERROR, "Thất bại !", "Từ này đã tồn tại trong từ điển.");
         gtk_widget_destroy(window_add);
         return;
     }
-    sprintf(sql, "INSERT INTO Dictionary VALUES(\"%s\",\"%s\");", word, nghia);
+    sprintf(sql, "INSERT INTO Dictionary VALUES(\"%s\",\"%s\");", word, meaning);
     sqlite3_exec(db, sql, 0, 0, &err_msg);
     show_message(window_add, GTK_MESSAGE_INFO, "Thành công !","Từ mới đã được thêm vào từ điển.");
     gtk_widget_destroy(window_add);
@@ -198,11 +199,11 @@ void press_edit()
     window_edit = GTK_WIDGET(gtk_builder_get_object(builder, "window_edit"));
     gtk_builder_connect_signals(builder, NULL);
 
-    entry2 = GTK_WIDGET(gtk_builder_get_object(builder, "entry2"));
-    textview4 = GTK_WIDGET(gtk_builder_get_object(builder, "textview4"));
+    entry_edit = GTK_WIDGET(gtk_builder_get_object(builder, "entry_edit"));
+    textview_edit = GTK_WIDGET(gtk_builder_get_object(builder, "textview_edit"));
 
     strcpy(word,gtk_entry_get_text(GTK_ENTRY(searchentry)));
-    gtk_entry_set_text(GTK_ENTRY(entry2),word);
+    gtk_entry_set_text(GTK_ENTRY(entry_edit),word);
     
     g_object_unref(builder);
     gtk_widget_show(window_edit); 
@@ -213,19 +214,19 @@ void click_ok_edit()
     GtkTextIter st_iter;
     GtkTextIter ed_iter;
     char word[50];
-    strcpy(word,gtk_entry_get_text(GTK_ENTRY(entry2)));
-    gtk_text_buffer_get_start_iter (gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview4)), &st_iter);
-    gtk_text_buffer_get_end_iter (gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview4)), &ed_iter);
-    char * nghia =  gtk_text_buffer_get_text(gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview4)), &st_iter, &ed_iter, FALSE);
+    strcpy(word,gtk_entry_get_text(GTK_ENTRY(entry_edit)));
+    gtk_text_buffer_get_start_iter (gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview_edit)), &st_iter);
+    gtk_text_buffer_get_end_iter (gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview_edit)), &ed_iter);
+    char * meaning =  gtk_text_buffer_get_text(gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview_edit)), &st_iter, &ed_iter, FALSE);
     exist = NULL;
     sprintf(sql,"SELECT * FROM Dictionary WHERE key_word = \"%s\" ;" ,word);
     sqlite3_exec(db, sql, callback_search, 0, &err_msg);
     if ( exist == NULL ) {
-       show_message(window_edit, GTK_MESSAGE_ERROR, "Xảy ra lỗi!", "Không có từ này trong từ điển.");
+       show_message(window_edit, GTK_MESSAGE_ERROR, "Thất bại !", "Không có từ này trong từ điển.");
        gtk_widget_destroy(window_edit);
        return;
     }
-    sprintf(sql, "UPDATE Dictionary SET meaning = \"%s\" WHERE key_word = \"%s\" ;", nghia, word);
+    sprintf(sql, "UPDATE Dictionary SET meaning = \"%s\" WHERE key_word = \"%s\" ;", meaning, word);
     sqlite3_exec(db, sql, 0, 0, &err_msg);
     show_message(window_edit, GTK_MESSAGE_INFO, "Thành công !", "Cập nhật từ thành công.");
     gtk_widget_destroy(window_edit);
@@ -242,10 +243,10 @@ void press_delete()
     window_delete = GTK_WIDGET(gtk_builder_get_object(builder, "window_delete"));
     gtk_builder_connect_signals(builder, NULL);
 
-    entry3 = GTK_WIDGET(gtk_builder_get_object(builder, "entry3"));
+    entry_delete = GTK_WIDGET(gtk_builder_get_object(builder, "entry_delete"));
 
     strcpy(word,gtk_entry_get_text(GTK_ENTRY(searchentry)));
-    gtk_entry_set_text(GTK_ENTRY(entry3),word);
+    gtk_entry_set_text(GTK_ENTRY(entry_delete),word);
     
     g_object_unref(builder);
     gtk_widget_show(window_delete); 
@@ -253,12 +254,12 @@ void press_delete()
 
 void click_ok_delete() {
     char word[50];
-    strcpy(word,gtk_entry_get_text(GTK_ENTRY(entry3)));
+    strcpy(word,gtk_entry_get_text(GTK_ENTRY(entry_delete)));
     exist = NULL ;
     sprintf(sql,"SELECT * FROM Dictionary WHERE key_word = \"%s\" ;" ,word);
     sqlite3_exec(db, sql, callback_check, 0, &err_msg);
     if (exist == NULL) {
-        show_message(window_delete, GTK_MESSAGE_ERROR, "Xảy ra lỗi!", "Không tồn tại trong từ điển.");
+        show_message(window_delete, GTK_MESSAGE_ERROR, "Thất bại !", "Không có từ này trong từ điển.");
         gtk_widget_destroy(window_delete);
         return;
     }
@@ -312,9 +313,7 @@ int callback_suggest(void *Notused, int argc, char **argv, char **azColName) {
   if (count > 9) return 0;
   gtk_list_store_append(list, &Iter);
   gtk_list_store_set(list, &Iter, 0, argv[0], -1 );
-
   count++;
-
   return 0;
 }
 
@@ -356,9 +355,9 @@ int main(int argc, char *argv[])
 
 
     // khoi tao bien tuong tac
-    textview1 = GTK_WIDGET(gtk_builder_get_object(builder, "textview1"));
-    textview2 = GTK_WIDGET(gtk_builder_get_object(builder, "textview2"));
-    searchentry=GTK_WIDGET(gtk_builder_get_object(builder, "searchentry1"));
+    textview_meaning = GTK_WIDGET(gtk_builder_get_object(builder, "textview_meaning"));
+    textview_history = GTK_WIDGET(gtk_builder_get_object(builder, "textview_history"));
+    searchentry=GTK_WIDGET(gtk_builder_get_object(builder, "searchentry"));
 
     // khoi tao phan goi y tu
     complete = gtk_entry_completion_new();
